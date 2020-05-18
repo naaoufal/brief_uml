59543b3f5d9d3e9ce093bf976fe8d2d9b3de6abf
= <=>
2020-05-18 18:56:26 +0000

    add files

---
+++ users.sql
@@ -0,0 +1,10 @@
+CREATE USER 'user_1'@'localhost' IDENTIFIED BY 'azer';
+GRANT ALL PRIVILEGES ON * . * TO 'user_1'@'localhost';
+FLUSH PRIVILEGES;
+
+
+
+
+CREATE USER 'user_2'@'localhost' IDENTIFIED BY '1234';
+GRANT CREATE, SELECT ON * . * TO 'user_2'@'localhost';
+FLUSH PRIVILEGES;
